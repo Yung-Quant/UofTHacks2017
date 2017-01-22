b707e16795c682c486a03eb5817801172bf4acc2
Omar Sandarusi <omar.sandarusi@gmail.com>
2017-01-22 05:31:47 -0500

    Rolled app.js to new user-input model

--- chatbot/app.js
+++ chatbot/app.js
@@ -7,7 +7,7 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 });
 
 var connector = new builder.ChatConnector({
-    appId: '72e12202-a840-462f-85f8-9e993d0bfdfd',//process.env.MICROSOFT_APP_ID,
+    appId: 'd2374428-8187-48f6-a248-a7d0d8b21aad',//process.env.MICROSOFT_APP_ID,
     appPassword: 'U04vvCGsr1seaDrOm5U7Fvd'//process.env.MICROSOFT_APP_PASSWORD
 });
 
@@ -18,41 +18,55 @@ server.post('/api/messages', connector.listen());
     session.send('Hello World');
 });*/
 
-var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/72e12202-a840-462f-85f8-9e993d0bfdfd?subscription-key=9be2351f2e23465ba1663a214a0fe997&verbose=true';
+var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/02ef0317-40d0-443a-ba13-3206060ecad2?subscription-key=9be2351f2e23465ba1663a214a0fe997&verbose=true';
 var recognizer = new builder.LuisRecognizer(model);
-var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
-bot.dialog('/', dialog);
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send('Welcome to the soccer prediction pool. What\'s your name?');
+});
 
-dialog.matches('CalculateProbability', [
-    function(session, args, next) {
-        var Team1 = builder.EntityRecognizer.findEntity(args.entities, 'team1');
-        var Team2 = builder.EntityRecognizer.findEntity(args.entities, 'team2');
-        //var Outcome = buider.EntityRecognizer.findEntity(args.entities, 'outcome');
-        if (!Team1 && !Team2) {
-            builder.Prompts.text(session, "No teams found.");
-        } else if (Team1 && !Team2) { 
-            builder.Prompts.text(session, "Team 1: " + Team1);
-        } else if (!Team1 && Team2) {
-            builder.Prompts.text(session, "Team 2: " + Team2);
+bot.dialog('loop', [
+    function (session, results) {
+        if (!session.userData.name) {
+            session.userData.name = results.response;
+            builder.Prompts.text(session, "Hi " + name + ". What team are you rooting for?");
         } else {
-            builder.Prompts.text(session, "Test 1: " + Team1 + ", Team 2: " + Team2);
-            /*next({ response: {
-                    toLocation: toLocation,
-                    fromLocation: fromLocation
-            } 
-            });*/
+            builder.Prompts.choice(session, "What team will you root for this time?", teams);
         }
-    }/*,
-    function(session, results) {
-        if (results.response) {
-            if (results.response.toLocation && results.response.fromLocation) {
-                session.send("Your flight has been booked from '%s' to '%s'.", results.response.toLocation, results.response.fromLocation);
-                results.response.toLocation = null;
-                results.response.fromLocation = null;
-            } else {
-                session.send("Ok");
-            }
-        }   
-    }*/
+    },
+    function (session, results) {
+        teams2 = removeByValue(results.response);
+        session.dialogData.firstChoice = results.response;
+        builder.Prompts.choice(session, "Who should we simulate them against?", teams2);
+    },
+    function (session, results) {
+        session.send('Simulating ' + session.dialogData.firstChoice + ' vs. ' + results.response);
+        //python call with session.dialogData.firstChoice and results.response
+    }
 ]);
-dialog.onDefault(builder.DialogAction.send("Default"));
+
+var teams = [
+    "KSV Cercle Brugge",
+    "Royal Excel Mouscron",
+    "Birmingham City",
+    "FC Nantes",
+    "SM Caen",
+    "Valenciennes FC",
+    "Stade de Reims",
+    "AC Arles-Avignon",
+    "Hannover 96",
+    "SC Freiburg",
+    "Portimonense",
+    "Manchester United",
+    "Manchester City",
+    "Liverpool",
+    "Tottenham Hotspur",
+    "Chelsea"
+];
+
+function removeByValue(value, teams) {
+    var index = teams.indexOf(value);
+    var newTeams = teams.slice();
+    newTeams.splice(index, 1);
+    return newTeams;    
+} 
+
