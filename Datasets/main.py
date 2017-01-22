91b55dec0233d46373a514b835a74818ff38cd23
Shane Kennedy <shaneikennedy@Shanes-MacBook-Pro.local>
2017-01-22 00:23:58 -0500

    add final output

--- Datasets/main.py
+++ Datasets/main.py
@@ -10,5 +10,12 @@ prob1 = first_layer.firstLayerMain(team1,team2) # first probability layer, head 
 print prob1
 prob2 = second_layer.secondLayerMain(team1,team2) # second probability, indirect comparision using mutual opponents
 print prob2
-prob3 = third_layer.thirdLayerMain(team1,team2) # probability boost, relative superiority in standings non-weighted just added 
-print prob3
+probBoost = third_layer.thirdLayerMain(team1,team2) # probability boost, relative superiority in standings non-weighted just added 
+print probBoost
+
+weight1 = 0.7
+weight2 = 0.3
+prob1Adj = prob1[0]*weight1
+prob2Adj = prob2[0]*weight2
+finalProb = prob1Adj + prob2Adj + probBoost
+print(finalProb)
